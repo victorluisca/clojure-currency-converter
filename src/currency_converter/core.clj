6b489dd59e415054648a7203e29a40cc0793567f
Victor Luís <victorluiscaguiar@gmail.com>
2025-12-22 02:25:17 -0300

    improve get-json to support multi-arity and add currency list fetching

--- src/currency_converter/core.clj
+++ src/currency_converter/core.clj
@@ -1,15 +1,20 @@
 (ns currency-converter.core
   (:require [clojure.tools.cli :refer [parse-opts]]
-            [clj-http.client :as http]))
+            [clj-http.client   :as http]
+            [clojure.string    :as str]))
 
 (defn get-json
-  [url params]
-  (try
-    (let [response (http/get url {:query-params params :as :json})]
-      (:body response))
-    (catch Exception e
-      {:error :http-error
-       :message (.getMessage e)})))
+  ([url]
+   (get-json url {}))
+  ([url params]
+   (try
+     (let [opts (cond-> {:as :json}
+                  (seq params) (assoc :query-params params))
+           response (http/get url opts)]
+       (:body response))
+     (catch Exception e
+       {:error   :http-error
+        :message (.getMessage e)}))))
 
 (defn fetch-rates
   [amount from to]
@@ -18,12 +23,21 @@
              :from   from
              :to     to}))
 
+(defn fetch-currencies
+  []
+  (get-json "https://api.frankfurter.dev/v1/currencies"))
+
 (def cli-options
-  [["-f" "--from CURRENCY" "Source currency code"]
-   ["-t" "--to CURRENCY"   "Target currency code"]
+  [["-f" "--from CURRENCY" "Source currency code"
+    :default "USD"
+    :parse-fn #(str/upper-case %)]
+   ["-t" "--to CURRENCY"   "Target currency code"
+    :default "BRL"
+    :parse-fn #(str/upper-case %)]
    ["-l" "--list"          "List all supported currencies"]
    ["-h" "--help"]])
 
 (defn -main
   [& args]
-  (parse-opts args cli-options))
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    (println options arguments errors summary)))
